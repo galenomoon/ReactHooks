34b62526edc1961cb81ae76a06c98e318bc535e4
galeno <gui.galenocover@gmail.com>
2022-01-14 14:01:37 -0300

    Rendering Counter with useRef()

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState, useRef, useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseRef = (props) => {
-    return (
-        <div className="UseRef">
-            <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
-            />
-        </div>
-    )
-}
 
-export default UseRef
+  const [value1, setValue1] = useState("");
+  const count = useRef(0) //.current
+
+
+  useEffect(function(){
+      count.current = count.current+1
+  }, [value1])
+
+  return (
+    <div className="UseRef">
+      <PageTitle
+        title="Hook UseRef"
+        subtitle="Retorna um objeto mutável com a propriedade .current!"
+      />
+      <SectionTitle title="Exercício #01" />
+      <div>
+        <span className="text">Value:</span>
+        <span className="text"> {value1} [</span>
+        <span className="text red">{count.current}</span>
+        <span className="text">]</span>
+        </div>
+      <div className="center">
+        <input
+          type="text"
+          className="input"
+          value={value1}
+          onChange={(e) => setValue1(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UseRef;
