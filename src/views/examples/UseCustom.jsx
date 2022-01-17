fe44f76bee87a9aada12ebbd35b2a3a12a29e20d
galeno <gui.galenocover@gmail.com>
2022-01-17 13:24:43 -0300

    Creating my Custom Hook using Fetch

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 import { useCounter } from "../../hooks/useCounter";
+import { useFetch } from "../../hooks/useFetch";
 
 const UseRef = (props) => {
-    
   const [count, inc, dec] = useCounter();
+  const url = "http://files.cod3r.com.br/curso-react/estados.json";
+  const response = useFetch(url);
+
+  function showStates(states) {
+    return states.map((state) => (
+      <li key={state.nome}>
+        {state.nome} - {state.sigla}
+      </li>
+    ));
+  }
 
   return (
     <div className="UseCustom">
@@ -25,6 +35,10 @@ const UseRef = (props) => {
             -1
           </button>
         </div>
+      </div>
+      <SectionTitle title="Exercise #02" />
+      <div className="center">
+        <ul>{response.data ? showStates(response.data) : false}</ul>
       </div>
     </div>
   );
